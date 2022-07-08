c33bebd98eebb74fa9aa289602e916b7469f6aa5
Shibin-b <shibinbevin123@gmail.com>
2022-07-08 12:24:26 +0530

    viewCart component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewCartComponent } from './view-cart/view-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewCartComponent
   ],
   imports: [
     BrowserModule,
